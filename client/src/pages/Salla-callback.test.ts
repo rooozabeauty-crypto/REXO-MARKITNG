28b4598ddcb989a32e5100d042cf14ed83640177
رموز احمد <rooozabeauty@gmail.com>
2026-04-14 19:43:34 +0300

    Update Salla-callback.test.ts

--- client/src/pages/Salla-callback.test.ts
+++ client/src/pages/Salla-callback.test.ts
@@ -32,7 +32,7 @@ describe("Salla OAuth Callback", () => {
   });
 
   it("يجب أن يكون Callback URL صحيحاً", () => {
-    const callbackUrl = "https://rexo-markitng.onrender.com/callback
+    const callbackUrl = "https://rexo-markitng.onrender.com/callback/
     expect(callbackUrl).toContain("rexomarketing");
     expect(callbackUrl).toContain("callback");
     expect(callbackUrl).toMatch(/^https:\/\//);
